a45bbe93d1985722ecdd282f9b18916479c6b667
tanyf5 <leehmann05@gmail.com>
2024-06-22 21:23:33 +0200

    hw4-1

---
+++ js/task-3.js
@@ -0,0 +1,23 @@
+const profile = {
+    username: "Jacob",
+    changeUsername(newName) {
+        this.username = newName;
+    },
+    playTime: 300,
+    updatePlayTime(hours) {
+        this.playTime += hours;
+    },
+    getInfo() {
+        return `${this.username} has ${this.playTime} active hours!`
+    },
+}
+
+
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
